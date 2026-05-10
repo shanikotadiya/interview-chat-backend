937b7b97bb4bacbe142bebfa8b739a91502b745b
Shani Kotadiya <shanikotadiya@gmail.com>
2026-05-10 11:43:20 +0530

    refactor rate limiter to simplify timestamp handling and improve performance

--- src/config/ratelimiter.js
+++ src/config/ratelimiter.js
@@ -10,25 +10,19 @@ const ratelimiter = async (req, res, next) =>{
    
 const ip = req.ip === "::1" ? "127.0.0.1" : req.ip;    console.log("ip", ip)
     let now  = Date.now()
-    const WindowSize = 60 * 1000
+    const WindowSize = 60
     const maxRequest = 5
 
-   let timestamp = JSON.parse(await client.get(ip)) || []
-    timestamp = timestamp.filter((tp)=>{
-       return now - tp < WindowSize
-    })
-    if(timestamp.length >= maxRequest){
+   const count  = await client.incr(ip)
+   if( count ===1 ){
+    await client.expire(ip, WindowSize)
+   }
+    if(count >= maxRequest){
         return res.json({
             message: "Too Many request" 
         })
     }
 
-    timestamp.push(now)
-
-    await client.set(ip, JSON.stringify(timestamp),{
-        EX:50
-    }),
-    
     next()
      } catch (error) {
        console.log(error) 
